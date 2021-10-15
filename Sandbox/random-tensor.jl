bc28f2b2b1c66eec89e25b9f65987fa7a12ddd0d
Amaury V. Miniño <amaury.minino@colostate.edu>
2021-10-15 13:51:47 -0600

    adjuted equality of characters

--- Sandbox/random-tensor.jl
+++ Sandbox/random-tensor.jl
@@ -2,16 +2,16 @@ module TensorSpace
 
 using Random #calling random numbers
 
-function rand-mat(v,F) #Gives a real or complex l-valence tensor, expressed as an array
+function rand_mat(v,F) #Gives a real or complex l-valence tensor, expressed as an array
     #if typeof(v) # I want a check to mmake sure I get an array of positive integers
-    if F=C #Complex
+    if F==="C" #Complex
         T=rand(ComplexF64,v) #gives an array with valence size(v) and and dimensions equal to the values of v
         return T
-    end
-    elseif F=R #Real
+    
+    elseif F==="R" #Real
         T=rand(Float64,v)
         return T
-    end
+    
     else
         print("Please specify your field as R (for real) or C(for complex)")
     end
