be7bc1961f7bec546f2784171876a6f900542a55
Amaury V. Miniño <amaury.minino@colostate.edu>
2021-10-15 16:17:34 -0600

    changed comments and field input

--- Sandbox/random-tensor.jl
+++ Sandbox/random-tensor.jl
@@ -2,18 +2,19 @@ module TensorSpace
 
 using Random #calling random numbers
 
-function rand_mat(v,F) #Gives a real or complex l-valence tensor, expressed as an array
-    #if typeof(v) # I want a check to mmake sure I get an array of positive integers
-    if F==="C" #Complex
-        T=rand(ComplexF64,v) #gives an array with valence size(v) and and dimensions equal to the values of v
+function rand_mat(v,F) #Gives a real or complex length(v)-valence tensor, expressed as an array
+    #v is a Tuple of positive integers i.e. (1,2,3), F is either the string "R" or "C"
+    #if typeof(v)=Tuple # I want a check to make sure I get a Tuple of positive integers
+    if F==='C' #Complex
+        T=rand(ComplexF64,v)
         return T
     
-    elseif F==="R" #Real
+    elseif F==='R' #Real
         T=rand(Float64,v)
         return T
     
     else
-        print("Please specify your field as R (for real) or C(for complex)")
+        print("Please specify your field as 'R' (for real) or 'C' (for complex)")
     end
 end# End of rand-mat
 
