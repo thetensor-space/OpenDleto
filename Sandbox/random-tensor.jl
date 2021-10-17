dc95b2ffdd5b2f8d1d09bc0a016bba09b3d216a3
Amaury V. Miniño <amaury.minino@colostate.edu>
2021-10-16 21:28:53 -0600

    Added integral tensors

--- Sandbox/random-tensor.jl
+++ Sandbox/random-tensor.jl
@@ -2,8 +2,10 @@ module TensorSpace
 
 using Random #calling random numbers
 
-function rand_mat(v,F) #Gives a real or complex length(v)-valence tensor, expressed as an array
-    #v is a Tuple of positive integers i.e. (1,2,3), F is either the string "R" or "C"
+function rand_tensor(v,F,I=-10:10) #Gives a real or complex length(v)-valence tensor, expressed as an array
+    #v is a Tuple of positive integers i.e. (1,2,3)
+    # F is either the string 'R', 'C', or 'Z'
+    #If the ring is the integers, I determines the integers that will populate the random tensor. writing Int will draw randomly from all integers.
     #if typeof(v)=Tuple # I want a check to make sure I get a Tuple of positive integers
     if F==='C' #Complex
         T=rand(ComplexF64,v)
@@ -13,6 +15,9 @@ function rand_mat(v,F) #Gives a real or complex length(v)-valence tensor, expres
         T=rand(Float64,v)
         return T
     
+    elseif F==='Z' #Integers
+        T=rand(I,v)
+        return T
     else
         print("Please specify your field as 'R' (for real) or 'C' (for complex)")
     end
