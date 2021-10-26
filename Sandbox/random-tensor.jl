994daf830194cdfd74350af4a2194e2315f2b6f0
Amaury V. Miniño <amaury.minino@colostate.edu>
2021-10-26 14:13:23 -0600

    Made the random numbers smaller

--- Sandbox/random-tensor.jl
+++ Sandbox/random-tensor.jl
@@ -2,24 +2,23 @@
 
 using Random #calling random numbers
 
-function randtensor(v,F='Z',I=-10:10) #Gives a real or complex length(v)-valence tensor, expressed as an array
+function randtensor(v,F='Z') #Gives a real or complex length(v)-valence tensor, expressed as an array
     #v is a Tuple of positive integers i.e. (1,2,3)
     # F is either the string 'R', 'C', or 'Z'
-    #If the ring is the integers, I determines the integers that will populate the random tensor. writing Int will draw randomly from all integers.
     #if typeof(v)=Tuple # I want a check to make sure I get a Tuple of positive integers
     if F==='C' #Complex
-        T=rand(ComplexF64,v)
+        T=rand(ComplexF8,v)
         return T
     
     elseif F==='R' #Real
-        T=rand(Float64,v)
+        T=rand(Float8,v)
         return T
     
     elseif F==='Z' #Integers
-        T=rand(I,v)
+        T=rand(Int8,v)
         return T
     else
-        print("Please specify your field as 'R' (for real) or 'C' (for complex)")
+        print("Please specify your field as 'R' (for real), 'C' (for complex), or 'Z' (for integers).")
     end
 end# End of rand-mat
 
