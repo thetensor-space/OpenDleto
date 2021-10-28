e61827c7b294b8f1f18f8914ed4be9d26efd61eb
Amaury V. Miniño <amaury.minino@colostate.edu>
2021-10-28 10:50:02 -0600

    fixed typos

--- Sandbox/test-Sylvester.jl
+++ Sandbox/test-Sylvester.jl
@@ -3,15 +3,15 @@ using TensorOperations
 A=randtensor((3,4,5),'Z')
 IA=('s','b','c')
 X1=randtensor((2,3),'Z')
-Ix1('a','s')
+IX1=('a','s')
 
 C1=zeros(Int8,2,4,5)
 IC1=('a','b','c')
 
-tensorcontract!(1,A,IA,'N',X2,IX2,'N',0,C1,IC1)
+tensorcontract!(1,A,IA,'N',X1,IX1,'N',0,C1,IC1)
 ##############################
 B=randtensor((2,7,5),'Z')
-IB('a','t','c')
+IB=('a','t','c')
 Y1=randtensor((7,4),'Z')
 IY1=('t','b')
 C2=zeros(Int8,2,4,5)
