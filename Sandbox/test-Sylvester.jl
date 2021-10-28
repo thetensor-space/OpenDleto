c6338493d9d0b489066a3087eb8eea823102881a
Amaury V. Miniño <amaury.minino@colostate.edu>
2021-10-28 15:43:41 -0600

    Fixed data type

--- Sandbox/test-Sylvester.jl
+++ Sandbox/test-Sylvester.jl
@@ -1,5 +1,7 @@
 #module TensorSpace
 using TensorOperations
+
+#XA
 A=randtensor((3,4,5),'Z')
 IA=('s','b','c')
 X1=randtensor((2,3),'Z')
@@ -9,7 +11,8 @@ C1=zeros(Int8,2,4,5)
 IC1=('a','b','c')
 
 tensorcontract!(1,A,IA,'N',X1,IX1,'N',0,C1,IC1)
-##############################
+
+#BY
 B=randtensor((2,7,5),'Z')
 IB=('a','t','c')
 Y1=randtensor((7,4),'Z')
@@ -21,9 +24,9 @@ tensorcontract!(1,B,IB,'N',Y1,IY1,'N',0,C2,IC2)
 ######################
 C=C1+C2
 
-X=sylvester(A,B,C)[1]
+X=sylvester(A,B,C,Int8)[1]
 
-Y=sylvester(A,B,C)[2]
+Y=sylvester(A,B,C,Int8)[2]
 
 
 
