019e4bfb0fa18f5d97ece1699be5c338288e10df
Amaury V. Miniño <amaury.minino@colostate.edu>
2021-11-10 22:38:14 -0700

    Began centroid function

---
+++ Sandbox/centroid.jl
@@ -0,0 +1,22 @@
+using LinearAlgebra
+#solving h(u*v)=f(u)*v=u*g(v)
+#ΓZ=XΓ=ΓY^T
+#Where
+#[ΓZ]_{ijk}=ΣΓ_{ijl}Z_{lk} = ΣZ_{il}Γ_{ljk} = ΣΓ_{ilk}Y_{jl}
+function cent(L)
+    I=size(L)[1]#X
+    J=size(L)[2]#Y
+    K=size(L)[3]#Z
+    M=zeros(I*J*K,I^2+J^2+K^2)
+    for i=1:I
+        for j=1:J
+            for k=1:K
+                index=i+I*((j-1)+J*(k-1))
+                M[index,i*I:(i+1)*I]= L[1:I,j,k] #X
+                M[index,I^2+j*J:(j+1)*I]=L[i,1:J,k]#Y
+                M[index,I^2+J^2+k*K:(k+1)*K]=L[i,j,1:K].*(-1)#Z^T, permuting columns for faster runtime
+            end
+        end
+    end
+    return M
+end
