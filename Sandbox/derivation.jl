69a71e8cb63b35b04805910c1527c6f399e64c64
algeboy <autotopism@gmail.com>
2021-11-11 21:16:23 -0700

    der

--- Sandbox/derivation.jl
+++ Sandbox/derivation.jl
@@ -3,20 +3,50 @@ using LinearAlgebra
 #ΓZ=XΓ+ΓY^T
 #Where
 #[ΓZ]_{ijk}=ΣΓ_{ijl}Z_{lk} = ΣZ_{il}Γ_{ljk} + ΣΓ_{ilk}Y_{jl} 
-function der(L)
-    I=size(L)[1]#X
-    J=size(L)[2]#Y
-    K=size(L)[3]#Z
-    M=zeros(I*J*K,I^2+J^2+K^2)
-    for i=1:I
-        for j=1:J
-            for k=1:K
-                index=i+I*((j-1)+J*(k-1))
-                M[index,i*I:(i+1)*I]= L[1:I,j,k] #X
-                M[index,I^2+j*J:(j+1)*I]=L[i,1:J,k]#Y
-                M[index,I^2+J^2+k*K:(k+1)*K]=L[i,j,1:K].*(-1)#Z^T, permuting columns for faster runtime
+
+
+function derRow(t, i,j,k)
+    F = eltype(t)
+    a = size(t)[1]; b = size(t)[2]; c = size(t)[3]
+    row = zeros(F,1,a^2+b^2+c^2)
+    # ΣZ_{il}Γ_{ljk}
+    copyto!(row, a*(i-1)+1, t[1:a,j,k], 1)
+    # ΣΓ_{ilk}Y_{jl}
+    copyto!(row, a^2+b*(j-1)+1, -t[i,1:b,k], 1)
+    # -ΣΓ_{ijl}Z_{lk}
+    copyto!(row, a^2+b^2+c*(k-1)+1, -t[i,j,1:c], 1)
+    return row
+end
+
+function der(t)
+    F = eltype(t)
+    a = size(t)[1]; b = size(t)[2]; c = size(t)[3]
+    M = zeros(F, (0,a^2+b^2+c^2) )
+    for k = axes(t,3)
+        for j = axes(t,2) 
+            for i = axes(t,1)
+                M = vcat(M,derRow(t, i,j,k))
             end
         end
     end
     return M
-end
+end 
+
+
+# function der(L)
+#     I=size(L)[1]#X
+#     J=size(L)[2]#Y
+#     K=size(L)[3]#Z
+#     M=zeros(I*J*K,I^2+J^2+K^2)
+#     for i=1:I
+#         for j=1:J
+#             for k=1:K
+#                 index=i+I*((j-1)+J*(k-1))
+#                 M[index,i*I:(i+1)*I]= L[1:I,j,k] #X
+#                 M[index,I^2+j*J:(j+1)*I]=L[i,1:J,k]#Y
+#                 M[index,I^2+J^2+k*K:(k+1)*K]=L[i,j,1:K].*(-1)#Z^T, permuting columns for faster runtime
+#             end
+#         end
+#     end
+#     return M
+# end
