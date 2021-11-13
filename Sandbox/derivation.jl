f450ab375ed2c849c46e77c9039b799d8ff6e32e
algeboy <autotopism@gmail.com>
2021-11-13 08:44:54 -0700

    BKW tests

--- Sandbox/derivation.jl
+++ Sandbox/derivation.jl
@@ -1,4 +1,11 @@
+########################################################################
+#  CC-BY 2021  Amaury V. Miniño and James B. Wilson.
+#    Distributed under MIT License.
+########################################################################
+
 using LinearAlgebra
+
+
 #solving d(u*v)=d(u)*v+u*d(v)
 #ΓZ=XΓ+ΓY^T
 #Where
@@ -39,22 +46,3 @@ function der(t)
     end
     return reshape( M, (step,a*b*c) )
 end 
-
-
-# function der(L)
-#     I=size(L)[1]#X
-#     J=size(L)[2]#Y
-#     K=size(L)[3]#Z
-#     M=zeros(I*J*K,I^2+J^2+K^2)
-#     for i=1:I
-#         for j=1:J
-#             for k=1:K
-#                 index=i+I*((j-1)+J*(k-1))
-#                 M[index,i*I:(i+1)*I]= L[1:I,j,k] #X
-#                 M[index,I^2+j*J:(j+1)*I]=L[i,1:J,k]#Y
-#                 M[index,I^2+J^2+k*K:(k+1)*K]=L[i,j,1:K].*(-1)#Z^T, permuting columns for faster runtime
-#             end
-#         end
-#     end
-#     return M
-# end
