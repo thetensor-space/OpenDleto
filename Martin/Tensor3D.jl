4bc6a9c15d282c1537d956c0556912d45ffe8f9a
kassabov <57142134+kassabov@users.noreply.github.com>
2021-11-20 19:01:27 -0500

    3D improvements
    improved the 3D output
    added graphing a function

--- Martin/Tensor3D.jl
+++ Martin/Tensor3D.jl
@@ -82,17 +82,29 @@ function print3D(t,ratio,factor)
 	raw = "x y z meta size\n"
     count = 0
     for i = axes(t,1)
+		vertst = ""
+		facest = ""
+		rawt =""
         for j = axes(t,2)
+			vertstt = ""
+			facestt = ""
+			rawtt = "" 
             for k = axes(t,3)
                 val = abs(t[i,j,k])
                 if (val > norm/ ratio) 
-                    verts *= printBoxCorner(i,j,k, val, norm,factor)
-                    faces *= printBoxFace(count)
-					raw *= string(i) * " " * string(j) * " " * string(k) * " 0 " * string(trunc(Int,ratio *val/norm )) * "\n"
+                    vertstt *= printBoxCorner(i,j,k, val, norm,factor)
+                    facestt *= printBoxFace(count)
+					rawtt *= string(i) * " " * string(j) * " " * string(k) * " 0 " * string(trunc(Int,ratio *val/norm )) * "\n"
                     count += 1
                 end
-            end 
+            end
+			vertst *= vertst
+			facest *= facestt
+			rawt *= rawtt
         end 
+		verts *= verts
+		faces *= facest
+		raw *= rawt
     end 
 
     ply = "ply\n"
@@ -125,3 +137,86 @@ function save3D(t, nameply,nameraw,ratio,factor)
     write(fileraw, raw)
     close(fileraw)
 end
+
+
+
+#Function the surface in the graph
+#s(x,y) = abs(((1-y)^2.5 - x^2.5 + 0.5))^0.4* sign((1-y)^2.5 - x^2.5 + 0.5)
+
+# produce a ply for a function
+#
+function graph3D(fun,xsize,ysize,zsize,step,name)
+	m = zeros(Int64,(step+1,step+1))
+	spoints = ""
+	sfaces = ""
+	pcount = 0
+	fcount = 0
+	for i = 1:(step+1)
+		s = ""
+		for j = 1:(step+1)
+			z = fun( (i-1)/step, (j-1)/step )
+			if ((z >= 0) && (z <= 1))
+				pcount +=1
+				m[i,j]= pcount
+				#generate point
+				s *= rstring(1+(i-1)*(xsize-1)/step) * " " * rstring(1+(j-1)*(ysize-1)/step) * " " * rstring(1+z*(zsize-1)) * " 255 0 0 \n"
+			end
+		end
+		spoints *= s
+	end
+	for i = 1:step
+		s = "" 
+		for j = 1:step
+			if (m[i,j] > 0)  && (m[i+1,j] > 0 ) && (m[i+1,j+1] > 0 ) && (m[i,j+1] > 0 )
+				fcount +=1
+				#generate face
+				s *= "4 " * string(m[i,j]-1) * " " * string(m[i+1,j]-1) * " " * string(m[i+1,j+1]-1) * " " * string(m[i,j+1]-1) * " \n"
+			else
+				if (m[i,j] > 0)  && (m[i+1,j] > 0 ) && (m[i+1,j+1] > 0 )
+					fcount +=1
+					#generate face
+					s *= "3 " * string(m[i,j]-1) * " " * string(m[i+1,j]-1) * " " * string(m[i+1,j+1]-1) * " \n"
+				end
+				if (m[i,j] > 0)  && (m[i+1,j] > 0 ) && (m[i,j+1] > 0 )
+					fcount +=1
+					#generate face
+					s *= "3 " * string(m[i,j]-1) * " " * string(m[i+1,j]-1) * " " * string(m[i,j+1]-1) * " \n"
+				end
+				if (m[i,j] > 0)  && (m[i+1,j+1] > 0 ) && (m[i,j+1] > 0 )
+					fcount +=1
+					#generate face
+					s *= "3 " * string(m[i,j]-1) * " " * string(m[i+1,j+1]-1) * " " * string(m[i,j+1]-1) * " \n"
+				end
+				if (m[i+1,j] > 0)  && (m[i+1,j+1] > 0 ) && (m[i,j+1] > 0 )
+					fcount +=1
+					#generate face
+					s *= "3 " * string(m[i+1,j]-1) * " " * string(m[i+1,j+1]-1) * " " * string(m[i,j+1]-1) * " \n"
+				end
+			end
+		end
+		sfaces *= s
+	end
+	#combine
+    ply = "ply\n"
+    ply *= "comment made by Magma  { Tensor rendered by Julia System }\n"
+    ply *= "format ascii 1.0\n"
+    ply *= "element vertex " * string(pcount) * "\n"
+    ply *= "property float x\n"
+    ply *= "property float y\n"
+    ply *= "property float z\n"
+    ply *= "property uchar red                   { start of vertex color }\n"
+    ply *= "property uchar green\n"
+    ply *= "property uchar blue\n"
+    ply *= "element face " * string(fcount) * "\n"
+    ply *= "property list uint8 int32 vertex_index\n"
+    ply *= "end_header\n"
+
+    ply *= spoints
+    ply *= sfaces
+
+    file = open(name, "w")
+    write(file, ply)
+    close(file)
+#    return ply
+end
+
